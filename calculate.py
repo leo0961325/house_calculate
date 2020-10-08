cc60ccaba3bcafc140ee30af61da042405fc46b1
leo <leo09613257@gmail.com>
2020-10-08 11:06:35 +0800

    add 建材

--- calculate.py
+++ calculate.py
@@ -1,5 +1,7 @@
 
-#初版:輸入建物資料可還原該土地每地坪價格。
+#版本1:輸入建物資料可還原該土地每地坪價格。
+#版本2:輸入建材條件
+
 
 class House_price :
 
@@ -21,9 +23,21 @@ class House_price :
 
     def the_age (self , *args) :
 
-        total_age = float(input('總年限:'))
-        used_age = float(input('屋齡:'))
+        total_age = input('耐用年限，請輸入1.鋼筋混凝土造 2.加強磚造 3. 木材造 :  ')
+        if total_age == '1' :
+            total_age = 50
+        elif total_age == '2' :
+            total_age = 35
+        elif total_age == '3' :
+            total_age = 20
+        else :
+            print('invalid input')
+            
+        used_age = float(input('現在屋齡:'))
         self.age =(total_age - used_age) /total_age
+
+        if used_age > total_age :
+            self.age = 0
 
         
     def formula (self) :
